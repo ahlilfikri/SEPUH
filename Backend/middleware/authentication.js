102d3f4cac38b21be0e3f048cd04faa76053883c
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-07-24 15:18:33 +0700

    feat/middleware fix auth

--- Backend/middleware/authentication.js
+++ Backend/middleware/authentication.js
@@ -7,7 +7,7 @@ const authenticateAdmin = (req, res, next) => {
     if (token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
-        if (user.role != 0) return res.sendStatus(403);
+        if (user.role != 2) return res.sendStatus(403);
         req.user = user;
         next();
     });
