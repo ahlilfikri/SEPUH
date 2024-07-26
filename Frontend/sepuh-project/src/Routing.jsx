851c6c27891b73977b5a7c9a32b2cd3f57f6d167
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-07-26 15:24:36 +0700

    give example

--- Frontend/sepuh-project/src/Routing.jsx
+++ Frontend/sepuh-project/src/Routing.jsx
@@ -1,16 +1,17 @@
 import { createBrowserRouter, RouterProvider, Route, Routes } from 'react-router-dom';
 import App from './App';
-import ProtectedRoute from './ProtectedRoute';
+import Home from './admin/pages/home';
+// import ProtectedRoute from './ProtectedRoute';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-    //   {
-    //     path: '/',
-    //     element: <Laporan />,
-    //   }
+      {
+        path: '/',
+        element: <Home />,
+      }
     ],
   },
 ]);
---
+++ Frontend/sepuh-project/src/admin/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from 'react';
+
+const Home = ({ toggleLoginPopup }) => {
+
+    return (
+        <Fragment>
+            <h1>TEST HOME</h1>
+        </Fragment>
+    );
+};
+
+export default Home;
