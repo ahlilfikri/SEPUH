920d802684b3082d36f5238dd6faf3b42bb1dc7a
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-07-30 14:27:06 +0700

    make navbar as component

--- Frontend/sepuh-project/src/admin/pages/home/index.jsx
+++ Frontend/sepuh-project/src/admin/pages/home/index.jsx
@@ -1,31 +1,15 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import Gambar from '/logo.svg';
+import React, { Fragment, useEffect, useState, useRef } from 'react';
 import Dokter from '/doksuster.png';
-import { useNavigate } from 'react-router-dom';
+import Navbar from '../../../shared/navbar';
 
 const Home = () => {
-    const navigate = useNavigate();
     const [isLogin, setIsLogin] = useState(false);
-
-    const handleSignin = () => {
-        navigate('/signin');
-    };
 
     const sectionRef = useRef(null);
 
     const handleScroll = () => {
-        
         sectionRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
     };
-
-
-    const handleProfile = () => {
-        navigate('/profile');
-    }
-    
-    const handleDashboard = () => {
-        navigate('/dashboard');
-    }
 
     const handleIsLogin = () => {
         const token = sessionStorage.getItem('token');
@@ -39,43 +23,7 @@ const Home = () => {
     return (
         <Fragment>
             <header>
-                <nav className="navbar navbar-expand-lg navbar-custom" style={{ background: 'linear-gradient(135deg, #225374, #28A09E)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>
-                    <div className="container-fluid">
-                        <a className="navbar-brand d-flex pe-5 pt-4" href="#">
-                            <img src={Gambar} alt="" width="32" height="24" className="d-inline-block align-text-top" />
-                            <p style={{ fontWeight: 'bold', marginLeft: '10px' }}>SEPUH</p>
-                        </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav ms-auto">
-                                <li className="nav-item">
-                                    {
-                                        !isLogin &&
-                                        <a className="nav-link active d-flex align-items-center pe-5 pt-4" aria-current="page" href="#">
-                                            <p style={{ fontWeight: 'bold', margin: 0, WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }} className="pe-4">Home</p>
-                                            <div onClick={handleSignin}>
-                                                <i className="fas fa-user" style={{ fontSize: '24px', color: '#28A09E' }}></i>
-                                                <span style={{ fontSize: '16px', fontWeight: '700', margin: '-10px 0 0 0' }} className='ps-1'>Signin</span>
-                                            </div>
-                                        </a>
-                                    }
-                                    {isLogin &&
-                                        <a className="nav-link active d-flex align-items-center pe-5 pt-4" aria-current="page" href="#">
-                                            <p style={{ fontWeight: 'bold', margin: 0, WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }} onClick={handleDashboard} className="pe-4">Dashboard</p>
-                                             <div onClick={handleProfile}>
-                                                <i className="fas fa-user" style={{ fontSize: '24px', color: '#28A09E' }}></i>
-                                                <span style={{ fontSize: '16px', fontWeight: '700', margin: '-10px 0 0 0' }} className='ps-1'>Profile</span>
-                                            </div>
-                                        </a>
-
-                                    }
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar isLogin={isLogin} />  {/* Use the Navbar component and pass isLogin as a prop */}
             </header>
             <main className="position-relative" style={{ minHeight: '100vh', background: 'linear-gradient(135deg, #225374, #28A09E)' }}>
                 <div className="container text-white" style={{ minHeight: '100vh' }}>
